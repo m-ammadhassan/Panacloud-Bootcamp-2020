25c084ce2309abb73d643d5a79265e9fb17d0a78
Muhammad Ammad Hassan <m.ammadhassan@yahoo.com>
2026-03-17 09:52:01 +0000

    Update App.jsx

--- Assignments/Assignment_03/src/App.jsx
+++ Assignments/Assignment_03/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Exercise01 from './components/Exercise 01/Exercise01'
 import Exercise02 from './components/Exercise 02/Exercise02'
 import Header from './components/Header'
-import Planets from './components/Planets'
+// import Planets from './components/Planets'
 
 const App = () => {
 
@@ -12,7 +12,7 @@ const App = () => {
       <main className="main padding">
         <Exercise01 />
         <Exercise02 />
-        <Planets />
+        {/* <Planets /> */}
       </main>
       
     </>
