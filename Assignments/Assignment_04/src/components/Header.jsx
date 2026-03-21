18afadc35196ee5bbc7e05a2c7815df3e4728ee8
Muhammad Ammad Hassan <m.ammadhassan@yahoo.com>
2026-03-21 06:18:47 +0000

    Assignment 04 Started

---
+++ Assignments/Assignment_04/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header flex text padding">
+        <h1>Assignment 04: State in React</h1>
+    </header>
+  )
+}
+
+export default Header
